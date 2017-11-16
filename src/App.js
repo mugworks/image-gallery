785a50b0ead21ccef60bad972584c9ace076c9af
mugworks <greenwood.pdx@gmail.com>
2017-11-15 19:46:51 -0800

    add page render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 import View from './View';
+// import Gallery from './Gallery';
 
 	
 class App extends Component {	
-  render(){
-    return(
-      <View/>
-    );
+  constructor() {
+    super();
+    this.state = {
+      bunnies: [
+        {
+          title: 'bunny1',
+          description: 'cute',
+          url: 'bunn.jpg',
+        }, 
+        {
+          title: 'bunny2',
+          description: 'great',
+          url: 'bunny.jpg',
+        },
+        {
+          title: 'bunny3',
+          description: 'good',
+          url: 'rabbit.jpg',
+        },
+      ],
+      view: 'list'
+    };
   }
+	
+	handleUpdateView = (view) => {
+	  console.log('handle updateview', view);
+	  this.setState({ view });	
+	}
+	
+	render(){
+	  return(
+	    <View bunnies={this.state.bunnies} view={this.state.view} handleUpdateView={this.handleUpdateView}/>
+	  // <Gallery/>
+	  );
+	}
 }
 export default App;
--- src/View.js
+++ src/View.js
@@ -1,48 +1,34 @@
 import React, { Component } from 'react';
 
 import List from './List';
+import Thumbnail from './Thumbnail';
+import Gallery from './Gallery';
 
 class View extends Component {
-  constructor() {
-    super();
-    this.state = {
-      bunnies: [
-        {
-          title: 'bunny1',
-          description: 'cute',
-          url: 'bunn.jpg',
-        }, 
-        {
-          title: 'bunny2',
-          description: 'great',
-          url: 'bunny.jpg',
-        },
-        {
-          title: 'bunny3',
-          description: 'good',
-          url: 'rabbit.jpg',
-        },
-      ],
-      view: 'list'
-    };
-  }
+  
 
-  //   handleOnClick
+  
   
   render() {
-    const bunnies = this.state.bunnies;
+		const { bunnies, view, handleUpdateView } = this.props;
+		let pageView;
+		if (view==='list') {pageView=(<List bunnies={bunnies}/>);}
+		if (view==='thumbnail') {pageView=(<Thumbnail bunnies={bunnies}/>);}
+		if (view==='gallery') {pageView=(<Gallery bunnies={bunnies}/>);}
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Cute Bunnies!</h1>
         </header>
-        <p className="App-intro">
+        <div>
+          <p className="App-intro">
             Select which view of cute bunnies you would like.
-        </p>
-        <button className="button">List View</button>
-        <button className="button">Thumbnail</button>
-        <button className="button">Gallery</button>
-        <List bunnies={bunnies}/>
+          </p>
+          <button className="button" onClick={() => handleUpdateView('list')}>List View</button>
+          <button className="button" onClick={() => handleUpdateView('thumbnail')}>Thumbnail</button>
+          <button className="button" onClick={() => handleUpdateView('gallery')}>Gallery</button>
+          { pageView }
+        </div>
       </div>
     );
   }
