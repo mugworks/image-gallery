f4b19855f472a55b779b57823939334d94da0994
mugworks <greenwood.pdx@gmail.com>
2017-11-20 19:46:37 -0800

    remove unused code

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import App from './App';
-import View from './View';
 import Gallery from './Gallery';
 import Thumbnail from './Thumbnail';
 import List from './List';
