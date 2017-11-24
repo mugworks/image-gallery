b972b6abef72d55c824346b8ffcf0f5b650152c6
mugworks <greenwood.pdx@gmail.com>
2017-11-24 14:11:25 -0800

    add proxy and move data to postman

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,39 @@
 import shortid from 'shortid';
 
 let store = {
-  albums = [],
-  albumsByid = {}
+  albums: [],
+  albumsByid: {}
 };
 
+let data = window.localStorage.getItem('store');
+if (data) store = JSON.parse(data);
+
+window.onbeforeunload = () => {
+  const json = JSON.stringify(store);
+  window.localStorage.setItem('data', json);
+};
+
+export const ListApi = {
+  get() {
+    return fetch('http://localhost:3002/api/images')
+      .then(response => response.json());
+  },
+  add(List) {
+    const saved = { ...list, _id: shortid.generate() };
+    store.lists.push(saved);
+    return Promise.resolve(saved);
+  },
+  remove(id) {
+    const index = store.lists.findIndex(l => l._id = id);
+    if (index !== -1) store.lists.splice(index, 1);
+    return Promise.resolve();
+  }
+};
+
+export const todoApi = {
+  get(listId) {
+    let todos = store.todosByListId[listId];
+    if(!todos) todos = store.todosByListId[listId] = [];
+    return Promise.resolve(todos.slice());
+  }
+}
